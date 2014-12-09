a404d45e108c7d13e58fc82f31a6aef636798e98
Raul Ferrandez <raul.ferrandez@here.com>
2014-12-09 17:07:18 +0100

    Continue menu

--- main.rb
+++ main.rb
@@ -9,8 +9,13 @@ def aboutBox
                     "Ruby/Tk release date :: tcltklib #{TclTkLib::RELEASE_DATE}; tk #{Tk::RELEASE_DATE}")
 end
 
+def seeProducts
+  Tk.messageBox('icon'=>'info', 'type'=>'ok', 'title'=>'Productes', 'message'=> "Todo ..." )
+end
 
-
+def seeCustomers
+  Tk.messageBox('icon'=>'info', 'type'=>'ok', 'title'=>'About Ecocity Demo', 'message'=> "To do ..." )
+end
 
 ########################################################################################################################
 ########################################## Start here ##################################################################
@@ -22,11 +27,22 @@ $root = TkRoot.new{ title "Porc Ecocity" }
 
 # See tk/menuspec.rb for menu_spec.
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/tk/rdoc/TkMenuSpec.html
-menu_spec = [[['File', 0],
-              ['About ... ', proc{aboutBox}, 0, '<F1>'],
-              '---',
-              ['Quit', proc{exit}, 0, 'Ctrl-Q']
-             ]]
+menu_spec = [
+              [
+                [ 'File', 0 ],
+                [ 'About ... ', proc{aboutBox}, 0, '<F1>' ],
+                '---',
+                [ 'Quit', proc{exit}, 0, 'Ctrl-Q' ]
+              ],
+              [
+                [ 'Productes', 0 ],
+                [ 'Veure Productes', proc{ seeProducts } ]
+              ],
+              [
+                ['Clients', 0],
+                ['Veure Clients', proc{ seeCustomers } ]
+              ]
+            ]
 
 # TkRoot::add_menubar(menu_spec, tearoff=false, opts=nil) click to tog)
 # opts is a hash of default configs for all of cascade menus.
