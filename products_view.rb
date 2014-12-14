c14907a5f2277698b8f3f6acb7e313215a76299f
Raul Ferrandez <ferrandezraul@gmail.com>
2014-12-14 18:01:42 +0100

    foo

--- products_view.rb
+++ products_view.rb
@@ -10,12 +10,11 @@ class ProductsView
   def initialize( args )
     @tree = Tk::Tile::Treeview.new( args[:parent]) {
       columns 'name price_tienda price_coope'
-
     }
 
     font = Ttk::Style.lookup( @tree[:style], :font )
-    cc = %w( name price_tienda price_coope )
-    cc.each{ |name|
+    columns = %w( name price_tienda price_coope )
+    columns.each{ |name|
       @tree.heading_configure( name, :text => name,
                              :command => proc{ sort_by( @tree, name, false ) } )
 
@@ -31,9 +30,24 @@ class ProductsView
     end
 
     ## Code to insert the data nicely
+    # root node in tree
+    @tree.insert( '', 'end', :id => 'products', :text => 'Productes')
+
+    # all nodes in tree as parent od
     args[:products].each{ | product |
-      @tree.insert( nil, :end, :values => [ product.name, product.price_tienda, product.price_coope ] )
+      # Inserted as children of node with :id => 'products' (root node)
+      @tree.insert( 'products', :end, :values => [ product.name, product.price_tienda, product.price_coope ] )
+
+      # Set column size based on length of data
+      # Extracted from ~/.rvm/src/ruby-2.1.1/ext/tk/sample/demos-en/widget
+      columns.zip( [product.name, product.price_tienda, product.price_coope] ).each{ | col, val |
+        len = TkFont.measure( font, "#{ val }  ")
+        if @tree.column_cget( col, :width ) < len
+          @tree.column_configure( col, :width => len )
+        end
+      }
     }
+
   end
 
   def grid(args)
@@ -47,7 +61,7 @@ class ProductsView
   end
 
   ## Code to do the sorting of the tree contents when clicked on
-  def sort_by(tree, col, direction)
+  def sort_by( tree, col, direction )
     @tree.children( nil ).map!{ |row| [@tree.get( row, col ), row.id] } .
         sort( &( (direction)? proc{ |x, y| y <=> x}: proc{ |x, y| x <=> y } ) ) .
         each_with_index{ | info, idx | @tree.move( info[1], nil, idx) }
