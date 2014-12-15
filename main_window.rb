b03e82dda198aad86a781e86b1df9247291e35d7
Raul Ferrandez <ferrandezraul@gmail.com>
2014-12-15 19:03:02 +0100

    foo

--- main_window.rb
+++ main_window.rb
@@ -10,44 +10,30 @@ class MainWindow
 
   def initialize(args)
     # root
-    root = TkRoot.new{ title "Porc Ecocity" }
+    @root = TkRoot.new{ title "Porc Ecocity" }
 
     # TkRoot::add_menubar(menu_spec, tearoff=false, opts=nil) click to tog)
     # opts is a hash of default configs for all of cascade menus.
     # Configs of menu_spec can override it.
-    root.add_menubar( menu_spec )
+    @root.add_menubar( menu_spec )
 
-    content = Tk::Tile::Frame.new(root)
+    @content = Tk::Tile::Frame.new(@root)
 
-    notebook = Tk::Tile::Notebook.new(content)
+    @notebook = Tk::Tile::Notebook.new(@content)
 
-    products_page = Tk::Tile::Frame.new(notebook)   # first page, which would get widgets gridded into it
-    customers_page = Tk::Tile::Frame.new(notebook)  # second page
+    @products_page = Tk::Tile::Frame.new( @notebook )   # first page, which would get widgets gridded into it
+    @customers_page = Tk::Tile::Frame.new( @notebook )  # second page
 
-    notebook.add products_page, :text => 'Products', :sticky => 'nswe'
-    notebook.add customers_page, :text => 'Customers', :sticky => 'nswe'
+    @notebook.add @products_page, :text => 'Products', :sticky => 'nswe'
+    @notebook.add @customers_page, :text => 'Customers', :sticky => 'nswe'
 
-    notebook.enable_traversal
+    @notebook.enable_traversal
 
-    products_view =  ProductsView.new( :parent => products_page,
+    @products_view =  ProductsView.new( :parent => @products_page,
                                        :products => args[:products] )
 
-    # All widgets are divided into columns and rows
-    # grid call makes the widget visible
-    # :sticky => nw When expanding, align it to north west
-    # :sticky => ew When expanding, align it to east west
-    content.grid :column => 0, :row => 0, :sticky => 'nsew'
-    notebook.grid :column => 0, :row => 0, :sticky => 'nsew'
-    products_view.grid :column => 0, :row => 0
-
-    # How expand is handled
-    # :weight => 1 expand widget when changing size
-    # Expand columns and rows with root
-    TkGrid.columnconfigure root, 0, :weight => 1; TkGrid.rowconfigure root, 0, :weight => 1
-
-    TkGrid.columnconfigure content, 0, :weight => 1; TkGrid.rowconfigure content, 0, :weight => 1
-
-    TkGrid.columnconfigure products_page, 0, :weight => 1; TkGrid.rowconfigure( products_page, 0, :weight => 1 )
+    # Draw widgets in grid
+    grid
 
     # start eventloop
     Tk.mainloop
@@ -90,6 +76,25 @@ class MainWindow
 
   def see_customers
     Tk.messageBox('icon'=>'info', 'type'=>'ok', 'title'=>'About Ecocity Demo', 'message'=> "TO do" )
+  end
+
+  def grid
+    # All widgets are divided into columns and rows
+    # grid call makes the widget visible
+    # :sticky => nw When expanding, align it to north west
+    # :sticky => ew When expanding, align it to east west
+    @content.grid :column => 0, :row => 0, :sticky => 'nsew'
+    @notebook.grid :column => 0, :row => 0, :sticky => 'nsew'
+    @products_view.grid :column => 0, :row => 0
+
+    # How expand is handled
+    # :weight => 1 expand widget when changing size
+    # Expand columns and rows with root
+    TkGrid.columnconfigure @root, 0, :weight => 1; TkGrid.rowconfigure @root, 0, :weight => 1
+
+    TkGrid.columnconfigure @content, 0, :weight => 1; TkGrid.rowconfigure @content, 0, :weight => 1
+
+    TkGrid.columnconfigure @products_page, 0, :weight => 1; TkGrid.rowconfigure( @products_page, 0, :weight => 1 )
   end
 
 end
